436e380799ef2728bfa27d34929b8bd7a449cf9f
Rocio Sanchez <20101172@mail.wit.ie>
2022-09-16 19:13:55 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -6,6 +6,7 @@ const aCar = {
         {owner:"Sheila Dwyer",  adress:"2 High Street"}
       ],
     type :{make : 'Toyota',model:'Corolla', cc:1.8} ,
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : {year:'201',countycode:'WD',number:'1058'}
   };
 
@@ -20,6 +21,12 @@ const aCar = {
   console.log(aCar.registration.year + '-' + aCar.registration.countycode +'-'+ aCar.registration.number)
 console.log("its a " + aCar.color.exterior +aCar.mileage +"mileage" + aCar.color.interior.shade +"interior")
 console.log('First owner : ' + aCar.previous_owners[0].owner+"  "+aCar.previous_owners[0].adress  )
+for (let i = 0 ; i < aCar.features.length ; i += 1) {
+  console.log(aCar.features[i]) ;
+} 
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+  console.log(aCar.previous_owners[i].owner+"  "+aCar.previous_owners[i].adress ) ;
+} 
 
 
   
