5c2970d9c75f4f54ccba710ac5bd779613dc705a
Rocio Sanchez <20101172@mail.wit.ie>
2022-09-16 18:50:26 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,8 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
     previous_owners: [
-        "Pat Smith - 1 Main Street",
-        "Sheila Dwyer - 2 High Street"
+        { owner:"Pat Smith", adress:" 1 Main Street"},
+        {owner:"Sheila Dwyer",  adress:"2 High Street"}
       ],
     type :{make : 'Toyota',model:'Corolla', cc:1.8} ,
     registration : {year:'201',countycode:'WD',number:'1058'}
@@ -18,7 +19,7 @@ const aCar = {
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log(aCar.registration.year + '-' + aCar.registration.countycode +'-'+ aCar.registration.number)
 console.log("its a " + aCar.color.exterior +aCar.mileage +"mileage" + aCar.color.interior.shade +"interior")
-console.log('First owner : ' + aCar.previous_owners[0] )
+console.log('First owner : ' + aCar.previous_owners[0].owner+"  "+aCar.previous_owners[0].adress  )
 
 
   
